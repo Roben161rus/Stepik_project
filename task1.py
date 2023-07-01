731d91df79cf3c87c35b879c976db606e242d574
Lifirenko <nikolay.lifirenko@mail.ru>
2023-07-01 13:07:26 +0300

    Добавил файл README и закончили уроки task1

--- task1.py
+++ task1.py
@@ -1,3 +1,23 @@
 # Выполнение заданий и описания их
-x = 5
-print(x)
+# x = 5
+# print(x)
+# print('*')
+# print('**')
+# print('***')
+# print('****')
+# print('*****')
+# print('******')
+# print('*******')
+
+name1 = input()
+name2 = input()
+name3 = input()
+
+print(name3)
+print(name2)
+print(name1)
+
+
+
+
+
